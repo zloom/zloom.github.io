0fdd071ff4d23a1a2a142624a660c4c7d6bfed89
Anton Zalialdinov <zloomsky@gmail.com>
2024-10-11 15:03:33 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
+  integrations: [tailwind(), icon(), mdx(), sitemap()],
   site: 'https://zloom.org'
 });
