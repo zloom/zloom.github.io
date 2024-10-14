0ce78df34e9ea5c12eccf570276e2804848228ad
Anton Zalialdinov <zloomsky@gmail.com>
2024-10-14 16:34:57 +0200

    fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx(), sitemap()],
-  site: 'https://zloom.org'
+  site: 'https://zloom.org',
+  integrations: [
+    tailwind(), 
+    icon(), 
+    mdx(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date('2024-10-14'),
+  })]
 });
