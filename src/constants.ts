8a3820f844e392f3078c44f25352901b8c6d7390
Anton Zalialdinov <zloomsky@gmail.com>
2024-11-05 23:36:16 +0200

    fixes og

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const sitename = 'Anton Zalialdinov, expert software engineer'
 export const imagealt = 'Anton Zalialdinov photo'
-export const about_description = 'Blog and portfolio focused on software development, NodeJs, .NET, Golang, Databases, DevOps, and other technical things.'
+export const about_description = 'Experienced software engineer with 10+ years across finance, digital, and global industries. Let’s solve complex problems and drive your business forward.'
 export const about_title = 'Anton Zalialdinov'
-export const contact_description = 'Send message or schedule a call'
+export const contact_description = 'Get in touch with me! Use the contact form, drop me an email, or schedule a call at your convenience.'
 export const contact_title = 'Contact me'
-export const blogs_description = 'Recent Anton Zalialdinov posts'
+export const blogs_description = 'Practical tips, lessons learned, and career insights from a software engineer’s journey.'
 export const blogs_title = 'Blogs'
 export const links = [
   "https://x.com/zloomsky",
