5802ff52fc6a0859e873b2132741c2b2edc0c239
Anton Zalialdinov <zloomsky@gmail.com>
2024-11-05 22:41:59 +0200

    formatting

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-export const sitename = 'Software engineer blog and portfolio'
+export const sitename = 'Anton Zalialdinov, expert software engineer'
 export const imagealt = 'Anton Zalialdinov photo'
 export const about_description = 'Blog and portfolio focused on software development, NodeJs, .NET, Golang, Databases, DevOps, and other technical things.'
 export const about_title = 'Anton Zalialdinov'
-export const contact_description = 'Anton Zalialdinov, send message or book a consultation'
-export const contact_title = 'Contact'
+export const contact_description = 'Send message or schedule a call'
+export const contact_title = 'Contact me'
 export const blogs_description = 'Recent Anton Zalialdinov posts'
 export const blogs_title = 'Blogs'
 export const links = [
