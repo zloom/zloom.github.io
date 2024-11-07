441a51deb0372405d2190a400ee4896f1a82ec0c
Anton Zalialdinov <zloomsky@gmail.com>
2024-11-07 18:52:54 +0200

    clean css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+const isDev = import.meta.env.DEV;
+
 export default defineConfig({
-  site: 'https://zloom.org',
+  site: isDev ? 'http://localhost:4321' : 'https://zloom.org',
   integrations: [
     tailwind(), 
     icon(), 
